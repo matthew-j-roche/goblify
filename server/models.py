f35afb515ad3a8cebad0f8ba034cc1a55246d2b6
matt roche <matt.roche@gmail.com>
2023-06-16 13:58:12 -0500

    added incomplete User model

--- server/models.py
+++ server/models.py
@@ -1,5 +1,20 @@
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
+from flask_bcrypt import Bcrypt
 
+# local imports
 from config import db
 
-# Models go here!
+class User(db.model, SerializerMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.integer, primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    created_at = db.Column(db.Column(db.DateTime, default=datetime.utcnow)
+
+    def to_dict(self)
+    return {
+        'id': self.id,
+        'username':self.username,
+        'created_at': self.created_at,;
+    }
