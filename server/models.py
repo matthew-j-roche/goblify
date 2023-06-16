90fd87c0547adbe04ded41e0716bb78126d377cd
NoFuture <brian.starrett@gmail.com>
2023-06-16 15:40:10 -0500

    added default values for models

--- server/models.py
+++ server/models.py
@@ -1,20 +1,103 @@
 from sqlalchemy_serializer import SerializerMixin
-from datetime import datetime
-from flask_bcrypt import Bcrypt
-
-# local imports
 from config import db
+from datetime import datetime
+from flask_bcrypt import bcrypt
+
 
 class User(db.model, SerializerMixin):
     __tablename__ = 'users'
     id = db.Column(db.integer, primary_key=True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
     username = db.Column(db.String(255))
     password = db.Column(db.String(255))
-    created_at = db.Column(db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime)
+    #current_neighborhood_id
+    #monster_name = db.Column(db.String(255))
+    
+    def set_password(self, password):
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
-    def to_dict(self)
-    return {
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)    
+
+    def to_dict(self):
+     return {
         'id': self.id,
-        'username':self.username,
-        'created_at': self.created_at,;
+        'first_name': self.first_name,
+        'last_name': self.last_name,
+        'username': self.username,
+        'password': self.password,
+        'created_at': self.created_at,
+        'updated_at': self.updated_at,
+     }
+
+class Neighborhood(db.model, SerializerMixin):
+   __tablename__ = 'neighborhoods'
+   id = db.Column(db.integer, primary_key=True)
+   notes = db.Column(db.String(255))
+   #favorited = db.Column(db.Boolean)
+
+   def to_dict(self):
+    return {
+       'id': self.id,
+       'notes': self.notes
     }
+
+class Games(db.model, SerializerMixin):
+   __tablename__ = 'games'
+   id = db.Column(db.integer, primary_key=True)
+   notes = db.Column(db.String(512))
+
+   def to_dict(self):
+    return {
+       'id': self.id,
+       'notes': self.notes
+    }
+
+class Games2Users(db.model, SerializerMixin):
+   __tablename__ = 'games2users'
+   id = db.Column(db.integer, primary_key=True)
+   game_id = db.Column(db.Integer,  db.ForeignKey('games.id'), nullable=False)
+   user_id = db.Column(db.Integer,  db.ForeignKey('users.id'), nullable=False)
+   completed_on = db.Column(db.DateTime)
+
+   def to_dict(self):
+    return {
+       'id': self.id,
+       'game_id': self.notes,
+       'user_id':self.user_id,
+       'completed_on':self.completed_on,
+    }   
+   
+class Locations(db.model, SerializerMixin):
+   __tablename__ = 'locations'
+   id = db.Column(db.integer, primary_key=True)
+   neighborhood_id = db.Column(db.Integer,  db.ForeignKey('neighborhoods.id'), nullable=False)
+   is_business = db.Column(db.Boolean)
+   notes = db.Column(db.String(512))
+
+   def to_dict(self):
+    return {
+       'id': self.id,
+       'neighborhood_id': self.neighborhood_id,
+       'is_business':self.is_business,
+       'notes':self.notes,
+    } 
+
+class Locations2Users(db.model, SerializerMixin):
+   __tablename__ = 'locations2users'
+   user_id = db.Column(db.Integer,  db.ForeignKey('users.id'), nullable=False)
+   location_id = db.Column(db.Integer,  db.ForeignKey('locations.id'), nullable=False)
+   is_tricked = db.Column(db.Boolean)
+   is_treated = db.Column(db.Boolean)
+
+   def to_dict(self):
+    return {
+       'id': self.id,
+       'user_id': self.user_id,
+       'location_id': self.location_id,
+       'is_tricked':self.is_tricked,
+       'is_treated':self.is_treated,
+    } 
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,86 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User, Neighborhood, Games, Games2Users, Locations, Locations2Users
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+
+        fake = Faker()
+        
+        fake.seed_locale('en_US', 0)
+
+
+        print("Deleting data...")
+        db.session.query(User).delete()
+        db.session.query(Neighborhood).delete()
+        db.session.query(Games).delete()
+        db.session.query(Games2Users).delete()
+        db.session.query(Locations).delete()
+        db.session.query(Locations2Users).delete()
+
+        print("Creating users...")
+        users = []
+        for _ in range(5):
+          first_name = fake.name()
+          last_name = fake.name()
+          password = fake.password()
+          username = fake.user_name()
+          created_at = fake.date_time_this_decade()
+          user = User(
+             first_name=first_name, 
+             last_name=last_name, 
+             username=username,
+             password=password, 
+             created_at=created_at)
+
+          users.append(user)
+          db.session.add(user)
+          db.session.commit()
+
+        print("Creating neighborhoods...")
+        neighborhoods = []
+        for _ in range(2):
+           neighborhood = Neighborhood()
+           neighborhoods.append(neighborhood)
+           db.session.add(neighborhood)
+           db.session.commit()
+
+        print("Creating games")
+        games = []
+        for _ in range(5):
+           game = Games()
+           games.append(game)
+           db.session.add(game)
+           db.session.commit()
+
+        print("Creating locations")
+        locations = []
+        for i in range(5):
+           location = Locations()
+           locations.append(location)
+           location.neighborhood_id = neighborhoods[i].id
+           db.session.add(location)
+           db.session.commit()
+
+
+        print("Mapping games to users...")
+        for i in range(2):
+           user = users[i]
+           game = games[i]
+           game2user = Games2Users(
+              user_id = user.id,
+              game_id = game.id
+           )
+
+        print("Mapping locations to users...")
+        for i in range(2):
+           location = locations[i]
+           user = users[i]
+           location2User = Locations2Users(
+              user_id = user.id
+              location_id = location.id
+           )
